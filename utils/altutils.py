a3a38b4ed3f15833c17d6c1dd9d9f47e28be3156
longvb <vuongbaolong48@gmail.com>
2021-11-07 23:50:51 +0700

    transforms.Resize((224,224))

--- utils/altutils.py
+++ utils/altutils.py
@@ -49,7 +49,7 @@ def get_ds(dataset_root, batch_size):
     """    
     # image pre-processing
     pre_process = transforms.Compose([
-        transforms.Resize(224),
+        transforms.Resize((224,224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
     ])
